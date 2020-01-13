5ccd405e7cfa08a468648b41a5040c0e08a30051
BlackWildBoar <815128782@qq.com>
2020-01-13 14:41:52 +0800

    初始化模块

---
+++ changgou_web/changgou_web_user/src/main/java/com/changgou/web/user/WebUserApplication.java
@@ -0,0 +1,11 @@
+package com.changgou.web.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebUserApplication.class, args);
+    }
+}
